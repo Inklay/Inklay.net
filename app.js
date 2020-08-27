f1c576802edb80ad84252225c6eb83d72bfc899d
Inklay <inklay@outlook.fr>
2020-08-27 22:27:03 +0200

    Removed old github webhooks

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express()
 const http = require('http').createServer(app)
 const port = 3000 ;
 const SyncHelper = require('./SyncHelper/SyncHelper.js');
-const cmd = require("node-cmd");
-const crypto = require("crypto");
-const bodyParser = require('body-parser')
 
-app.use(bodyParser.json());
 
 app.get("/SyncHelper/app/check_version", (req, res) => {
     SyncHelper.checkVersion(req, res);
@@ -17,32 +13,9 @@ app.get("/SyncHelper/app/sync", (req, res) => {
     SyncHelper.sync(req, res);
 });
 
-const verifySignature = (req, res, next) => {
-  const payload = JSON.stringify(req.body)
-  const hmac = crypto.createHmac('sha1', process.env.SECRET)
-  const digest = 'sha1=' + hmac.update(payload).digest('hex')
-  const checksum = req.headers['x-hub-signature']
-
-  if (!checksum || !digest || checksum !== digest) {
-    return res.status(403).send('auth failed')
-  }
-
-  return next()
-}
-
-app.post('/git', verifySignature, (req, res) => {
-  if (req.headers['x-github-event'] == 'push') {
-    cmd.get('bash git.sh', (err, data) => {
-      if (err) return console.log(err)
-      cmd.run('refresh')
-      return res.status(200).send(data)
-    })
-  } else if(req.headers['x-github-event'] == 'ping') {
-    return res.status(200).send('PONG')
-  } else {
-    return res.status(200).send('Unsuported Github event. Nothing done.')
-  }
-})
+app.get("/", (req, res) => {
+    res.status("200").send("Hello there");
+});
 
 http.listen(port, () => {
     console.log(`Server running on port ${port}`)
