26d6cca1b994081f8529bcbc4663037d0c7cb13c
Inklay <inklay@outlook.fr>
2020-08-27 07:59:35 +0200

    Added everything to pull from git

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000 ;
 const ets2sh = require('./ETS2SH/ETS2SH.js');
 const cmd = require("node-cmd");
 const crypto = require("crypto");
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 
 app.get("/ETS2SH/app/check_version", (req, res) => {
     ets2sh.checkVersion(req, res);
@@ -14,21 +17,32 @@ app.get("/ETS2SH/app/sync", (req, res) => {
     ets2sh.sync(req, res);
 });
 
-app.post('/git', (req, res) => {
-    let hmac = crypto.createHmac("sha1", process.env.SECRET);
-    let sig  = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex");
+const verifySignature = (req, res, next) => {
+  const payload = JSON.stringify(req.body)
+  const hmac = crypto.createHmac('sha1', process.env.SECRET)
+  const digest = 'sha1=' + hmac.update(payload).digest('hex')
+  const checksum = req.headers['x-hub-signature']
 
-    if (req.headers['x-github-event'] == "push" && sig == req.headers['x-hub-signature']) { 
-        cmd.run('chmod 777 git.sh');
-        cmd.get('./git.sh', (err, data) => {
-            if (data) console.log(data);
-            if (err) console.log(err);
-        });
-        cmd.run('refresh');
-        console.log("> [GIT] Updated with origin/master");
-    }
-    res.sendStatus(200);
-  });
+  if (!checksum || !digest || checksum !== digest) {
+    return res.status(403).send('auth failed')
+  }
+
+  return next()
+}
+
+app.post('/git', verifySignature, (req, res) => {
+  if (req.headers['x-github-event'] == 'push') {
+    cmd.get('bash git.sh', (err, data) => {
+      if (err) return console.log(err)
+      cmd.run('refresh')
+      return res.status(200).send(data)
+    })
+  } else if(req.headers['x-github-event'] == 'ping') {
+    return res.status(200).send('PONG')
+  } else {
+    return res.status(200).send('Unsuported Github event. Nothing done.')
+  }
+})
 
 http.listen(port, () => {
     console.log(`Server running on port ${port}`)
--- git.sh
+++ git.sh
@@ -1,7 +1,21 @@
-#/bin/sh
+#!/bin/sh
 
+if [ -z "$GITHUB_URL" ]; then
+  echo "Define GITHUB_URL in .env file"
+  exit 0
+fi
+
+origin_url=`git config --get remote.origin.url`
+
+if [ "$origin_url" != "$GITHUB_URL" ]; then
+  if [ -z "$(git remote | grep origin)" ]; then
+    git remote add origin $GITHUB_URL
+  else
+    git remote set-url origin $GITHUB_URL
+  fi
+fi
+
+git checkout master
 git fetch origin master
-
 git reset --hard origin/master
-
 git pull origin master --force
